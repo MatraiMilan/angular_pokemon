63b5f7171db8f368ac4d0dc32697eb2a9b0ef8ac
Pituk <212585852@logon.ds.ge.com>
2016-11-11 16:34:37 +0100

    angular html get and names to flexbox

--- src/pokemon/pokemon.js
+++ src/pokemon/pokemon.js
@@ -1,3 +1,11 @@
 /**
  * Created by 212585860 on 11/11/2016.
  */
+
+var app = angular.module('myapp', []);
+app.controller('pokemonsController', function($scope, $http) {
+    $http.get("http://pokeapi.co/api/v2/pokemon/?limit=721")
+        .then(function (response) {$scope.pokemons = response.data.results;});
+});
+
+
