766439e446e74667f64a3363af4d6e0d82878ff5
Pituk <212585852@logon.ds.ge.com>
2016-11-14 13:37:28 +0100

    Pokemons with images: working

--- src/pokemon/pokemon.js
+++ src/pokemon/pokemon.js
@@ -3,13 +3,28 @@
  */
 
 var app = angular.module('myapp', []);
+
 app.controller('pokemonsController', function($scope, $http, $rootScope) {
+
     $http.get("http://pokeapi.co/api/v2/pokemon/?limit=721")
-        .then(function (response) {$scope.pokemons = response.data.results;})
+        .then(function (response) {$scope.pokemons = response.data.results;});
+
     $rootScope.orderByMe = function (x) {
         $scope.myOrderBy = x;
-    }
-    ;
+
+    };
+
+    $scope.getPokeImgUrl = function (url){
+        var regxPattern = /\/\d+/;
+        var pokeId =  regxPattern.exec(url);
+        return "//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon" + (pokeId) + "\.png";
+    };
+
 });
+
+
+
+
+
 
 
