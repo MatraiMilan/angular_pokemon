17325aaa2e08809d8f9a7a4baeeb02f0affde552
Mátrai Milán <milan.matrai@ge.com>
2016-11-14 10:31:08 +0100

    -html from link

--- src/pokemon/pokemon.js
+++ src/pokemon/pokemon.js
@@ -4,7 +4,7 @@
 
 var app = angular.module('myapp', []);
 app.controller('pokemonsController', function($scope, $http, $rootScope) {
-    $http.get("http://pokeapi.co/api/v2/pokemon/?limit=721")
+    $http.get("//pokeapi.co/api/v2/pokemon/?limit=721")
         .then(function (response) {$scope.pokemons = response.data.results;})
     $rootScope.orderByMe = function (x) {
         $scope.myOrderBy = x;
